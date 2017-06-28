15dcffd094dbe6f8ead75b69052b5af8e38e7436
Yufeng Liu Reach <yufeng8@vt.edu>
2017-06-28 00:00:00 -0700

    message

--- generate.py
+++ generate.py
@@ -5,7 +5,7 @@ import subprocess
 import random
 
 # def update():
-#     date = datetime.date(2017, 6, 22)
+#     date = datetime.date(2017, 6, 28)
 #     return another_date
 
 # print(update())
@@ -29,7 +29,7 @@ def process(new_date):
     subprocess.check_output(['env', f'GIT_COMMITTER_DATE={currentTime}', 'git', 'commit', f'--date={currentTime}', '-am', 'message'])
 
 def main():
-    start_date = datetime.date(2017, 6, 22)
+    start_date = datetime.date(2017, 6, 28)
     end_date = datetime.date.today()
     while start_date < end_date:
         diff_date = datetime.timedelta(days=1)
